64453a9e83edce71bb116d95d511b0a1a9ac9b8e
EmiColeman780 <309199564+EmiColeman780@users.noreply.github.com>
2019-08-02 16:30:00 +0530

    perf: improve meetings

--- src/meetingtax/fragments.py
+++ src/meetingtax/fragments.py
@@ -68,3 +68,11 @@ def _merge_intervals(
     merged = [ordered[0]]
     for start, end in ordered[1:]:
         last_start, last_end = merged[-1]
+        if start <= last_end:
+            merged[-1] = (last_start, max(last_end, end))
+        else:
+            merged.append((start, end))
+    return merged
+
+
+def _clip(
