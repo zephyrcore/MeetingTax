0685aa8a00e0f802ffd18b2b326d95e06a2e2b4d
bastiaanpopov19 <309017743+bastiaanpopov19@users.noreply.github.com>
2019-11-13 18:24:00 +0530

    style: clean up fragmentation

---
+++ src/meetingtax/recurring.py
@@ -0,0 +1,8 @@
+"""Recurring series analysis.
+
+A recurring meeting is stale when it repeats without its shape ever changing:
+the same attendee set and the same duration on every instance. A weekly meeting
+that has run unchanged for months is the kind of standing invite worth
+questioning, because nobody has adjusted it to what the team now needs.
+"""
+
