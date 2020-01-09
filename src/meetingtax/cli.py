8aa75952705e525e3e69bf5893ceca5c191df413
Zephyr <36224422+zephyrcore@users.noreply.github.com>
2020-01-09 15:39:00 +0530

    fix: fragmentation

--- src/meetingtax/cli.py
+++ src/meetingtax/cli.py
@@ -12,3 +12,13 @@ Exit codes: 0 clean, 1 findings present, 2 usage error.
 from __future__ import annotations
 
 import argparse
+import sys
+
+from meetingtax import __version__
+from meetingtax import report as report_mod
+from meetingtax.events import expand
+from meetingtax.fragments import Workday, compute_all, summarise_people
+from meetingtax.ics import ICSError, parse_calendar
+from meetingtax.recurring import analyse
+
+EXIT_CLEAN = 0
