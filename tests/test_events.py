df8730a32e09496c44dfd5755a3da0cfec15e04e
francescobrouwer <309159529+francescobrouwer@users.noreply.github.com>
2021-01-15 13:24:00 +0530

    feat: fragmentation

--- tests/test_events.py
+++ tests/test_events.py
@@ -8,3 +8,10 @@ from meetingtax.events import expand
 
 
 def _cal(body: str) -> list:
+    return ics.parse_calendar("BEGIN:VCALENDAR\r\n" + body + "END:VCALENDAR\r\n")
+
+
+class ExpandTests(unittest.TestCase):
+    def test_single_meeting_expands_per_attendee(self):
+        events = _cal(
+            "BEGIN:VEVENT\r\n"
