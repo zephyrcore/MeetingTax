92a8aac03e934e3b0612b8659c8ce87cd345778c
Zephyr <36224422+zephyrcore@users.noreply.github.com>
2021-01-28 16:15:00 +0530

    fix: recurring

--- tests/test_events.py
+++ tests/test_events.py
@@ -15,3 +15,11 @@ class ExpandTests(unittest.TestCase):
     def test_single_meeting_expands_per_attendee(self):
         events = _cal(
             "BEGIN:VEVENT\r\n"
+            "UID:m@x\r\n"
+            "SUMMARY:Sync\r\n"
+            "DTSTART:20260907T090000\r\n"
+            "DTEND:20260907T093000\r\n"
+            "ORGANIZER;CN=Ada:mailto:ada@x\r\n"
+            "ATTENDEE;CN=Bao:mailto:bao@x\r\n"
+            "END:VEVENT\r\n"
+        )
