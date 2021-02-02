fbb2fbf0d93d08311f53d315a01b856a04995bc3
Zephyr <36224422+zephyrcore@users.noreply.github.com>
2021-02-02 19:36:00 +0530

    style: clean up cli

--- tests/test_fragments.py
+++ tests/test_fragments.py
@@ -5,3 +5,8 @@ import unittest
 
 from meetingtax.events import Occurrence
 from meetingtax.fragments import Workday, compute_day, compute_all, summarise_people
+
+
+def _occ(attendee, sh, sm, eh, em, day=dt.date(2026, 9, 7)):
+    start = dt.datetime(day.year, day.month, day.day, sh, sm)
+    end = dt.datetime(day.year, day.month, day.day, eh, em)
