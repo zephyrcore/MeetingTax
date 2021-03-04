48be17d780f31fa90cde2547e57bebcd6ee3a461
Zephyr <36224422+zephyrcore@users.noreply.github.com>
2021-03-04 18:15:00 +0530

    refactor: ics

--- tests/test_recurring.py
+++ tests/test_recurring.py
@@ -4,3 +4,8 @@ import unittest
 
 from meetingtax import ics
 from meetingtax.recurring import analyse, stale_series
+
+
+def _cal(body: str) -> list:
+    return ics.parse_calendar("BEGIN:VCALENDAR\r\n" + body + "END:VCALENDAR\r\n")
+
