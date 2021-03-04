376a27385b5685b195331fb1e08986ea6f1fb283
Zephyr <36224422+zephyrcore@users.noreply.github.com>
2021-03-04 18:20:00 +0530

    chore: bump focus

--- tests/test_recurring.py
+++ tests/test_recurring.py
@@ -9,3 +9,8 @@ from meetingtax.recurring import analyse, stale_series
 def _cal(body: str) -> list:
     return ics.parse_calendar("BEGIN:VCALENDAR\r\n" + body + "END:VCALENDAR\r\n")
 
+
+class RecurringTests(unittest.TestCase):
+    def test_weekly_series_is_stale(self):
+        events = _cal(
+            "BEGIN:VEVENT\r\n"
